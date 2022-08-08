9959349bd4163a2f388a6c8b81f166eed3c1a7d0
Leo Volwahsen <volwahsenl@gmail.com>
2022-08-08 14:15:11 +0200

    2 done

--- index.js
+++ index.js
@@ -2,53 +2,65 @@
 //1. Erstelle eine Übersicht mit unseren Usern. Wende hier dein Wissen über Objekte an, um eine möglichst effiziente Datenstruktur zu erreichen.
 
 ////////////////////////////////////////////////////////////////////////////////////////////
-const User1 = {
-    Vornamen: "Raja",
-    Nachnamen: "Underwood",
-    [Email - Adresse]: "pellentesque.tincidunt.tempus@outlook.org",
+const user1 = {
+    vornamen: "Raja",
+    nachnamen: "Underwood",
+    email: "pellentesque.tincidunt.tempus@outlook.org",
     Hobby1: "Musik",
     Hobby2: "IT",
     Hobby3: "Sport",
 };
-const User2 = {
-    Vornamen: "Owen",
-    Nachnamen: "Bennett",
-    [Email - Adresse]: "aliquet@protonmail.net",
+const user2 = {
+    vornamen: "Owen",
+    nachnamen: "Bennett",
+    email: "aliquet@protonmail.net",
     Hobby1: "Programmieren",
-    Hobby2: "Nachrichten",
+    Hobby2: "nachrichten",
     Hobby3: "Aktien",
 };
-const User3 = {
-    Vornamen: "Amery",
-    Nachnamen: "Conway",
-    [Email - Adresse]: "erat.neque.non@google.ca",
+const user3 = {
+    vornamen: "Amery",
+    nachnamen: "Conway",
+    email: "erat.neque.non@google.ca",
     Hobby1: "Reisen",
     Hobby2: "Festivals",
     Hobby3: "",
 };
-const User4 = {
-    Vornamen: "Lillian",
-    Nachnamen: "Clay",
-    [Email - Adresse]: "adipiscing.elit@aol.couk",
+const user4 = {
+    vornamen: "Lillian",
+    nachnamen: "Clay",
+    email: "adipiscing.elit@aol.couk",
     Hobby1: "Autos",
     Hobby2: "IT",
     Hobby3: "",
 };
-const User5 = {
-    Vornamen: "Nicholas",
-    Nachnamen: "Berg",
-    [Email - Adresse]: "nullam.feugiat@yahoo.net",
-    Hobby1: "Essen",
+const user5 = {
+    vornamen: "nicholas",
+    nachnamen: "Berg",
+    email: "nullam.feugiat@yahoo.net",
+    Hobby1: "essen",
     Hobby2: "",
     Hobby3: "",
 };
 console.log("Aufgabe1");
-//2. Beim Aufruf des Skripts soll eine Ausgabe erzeugt werden, die in etwa so aussieht:
-//"Peter Müller ist mit der Adresse petermueller@web.de registriert und erhält Newsletter zu den Themen Sport und Kochen.
-//Sarah Schmidt ist mit der Adresse sarah@schmidt.de registriert und erhält Newsletter zu den Themen Musik, IT und Reisen.
 
+//2. Beim Aufruf des Skripts soll eine Ausgabe erzeugt werden, die in etwa so aussieht:
+//"Peter Müller ist mit der Adresse petermueller@web.de registriert und erhält newsletter zu den Themen Sport und Kochen.
+//Sarah Schmidt ist mit der Adresse sarah@schmidt.de registriert und erhält newsletter zu den Themen Musik, IT und Reisen.
 ////////////////////////////////////////////////////////////////////////////////////////////
+
 console.log("Aufgabe2");
+console.log(
+    user1.vornamen,
+    user1.nachnamen,
+    "ist mit der Adresse",
+    user1.email,
+    "regestriert und erhält newsletter zu den Themen",
+    user1.Hobby1,
+    "und",
+    user1.Hobby2,
+    "."
+);
 //Zusatzaufgaben:
 //3. Berücksichtige den Fall, dass ein User kein Interessensgebiet angegeben hat und formuliere die Ausgabe hierfür passend um.
 //4. Nimm noch ein Registrierungsdatum mit auf und lass es ebenfalls ausgeben ("... ist mit der Adresse x seit y registriert...")
